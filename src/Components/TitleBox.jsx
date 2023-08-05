216b75f283b1df2fe18d4558e491ad72f2409f70
송수빈 <ttnqls0217@gmail.com>
2023-08-05 17:07:21 +0900

    delete button update

--- src/Components/TitleBox.jsx
+++ src/Components/TitleBox.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
 import DropDown from './DropDown';
 import Category from './Category';
 import Search from './Search';
 import TableComponent from './TableComponent';
 
 const TitleBox = () => {
+  
+
     return (
         <div className='frame'>
-            <div className='titleframe'>
+            <div className='firstbox'>
                 <span className='titletext'>동영상 리스트</span>
+                <button type='button' className='delete-button'>
+                    <span className='delete-text'>
+                        삭제 
+                    </span>
+                </button>
                     <div className='buttonframe'>
                         <DropDown />
                         <Category /> 
                         <Search />
                     </div>
-                        <TableComponent />
+                    <TableComponent/>
             </div>
-
         </div>
 
     );
